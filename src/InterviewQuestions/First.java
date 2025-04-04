e01cbe9d59d26ede21dbd92a9217eaf29a00215e
NikhilPal79 <nikhilchpr7@gmail.com>
2025-04-04 13:51:23 -0400

    interview

---
+++ src/InterviewQuestions/First.java
@@ -0,0 +1,11 @@
+package InterviewQuestions;
+
+public class First {
+    /// String vs StringBuilder vs StringBuffer
+    /// Singleton class
+    /// throw vs throws
+    /// method overloading vs overriding
+    /// runtime polymorphism vs compile time
+    /// checked vs unchecked exceptions
+
+}
