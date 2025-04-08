c81fd68a0912f09a02f47ee52271ffb1c28c5b39
NikhilPal79 <nikhilchpr7@gmail.com>
2025-04-07 21:34:54 -0400

    5th apr

---
+++ src/apr5/ClassAssign.java
@@ -0,0 +1,22 @@
+package apr5;
+
+import java.util.LinkedList;
+
+public class ClassAssign {
+    public static void main(String[] args) {
+
+
+        /*
+        ForLoops
+        Iterator
+        Create ListIterator
+        Use iterator for HashSet
+
+
+        Smallest and largest number in arraylist
+        Numbers greater than 10 and less than 10*/
+
+
+
+    }
+}
--- src/apr5/IteratorClass.java
+++ src/apr5/IteratorClass.java
@@ -1,13 +1,100 @@
 package apr5;
 
-import java.util.Iterator;
+import java.util.*;
+import sun.text.normalizer.UnicodeSetIterator;
 
 public class IteratorClass {
     public static void main(String[] args) {
 
 
-        ///  Iterator ===> it is a class
+        ///  Iterator ===> it is an interface
 
-        Iterator
+        ///  WHT THERE IS NEED OF ITERATOR ??
+
+        LinkedList<Integer> list =  new LinkedList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(60);
+        list.add(70);
+        list.add(80);
+
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println();
+
+        /*for (Integer e : list) {
+            if (list.contains(10)) {
+                list.remove();
+            }
+
+        }
+        System.out.println(list);*/
+
+        ///  THE ABOVE CODE IS GIVING US EXCEPTION BECAUSE WE ARE TRYING TO LOOP AND REMOVE{modifying} AT SAME TIME
+
+        ///  TO SOLVE THIS PROBLEM WE USE ITERATOR, or we can say to bypass the ConcurrentModification they create Iterator
+
+        /*if (list.contains(10)) {
+            list.remove(2);
+
+        }
+        System.out.println(list);*/
+
+        ///  ITERATOR
+
+
+        ///  Iterator basic
+
+
+        // BASIC ITERATOR
+
+        /* Iterator<Integer> itr = list.iterator();*/
+
+
+        ///  REMOVING THE ELEMENT
+        Iterator<Integer> itr = list.iterator();
+
+
+
+        while (itr.hasNext()){
+
+            Integer next = itr.next();
+
+            if(next == 30 || next == 70){
+                itr.remove();
+
+            }
+
+        }
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println();
+
+
+        ///  trying listIterator{not important }
+
+
+        ListIterator<Integer> listIterator = list.listIterator();
+
+        while (listIterator.hasNext()){
+            Integer next = listIterator.next();
+
+            if (next == 80 || next == 10) {
+                listIterator.remove();
+
+            }
+        }
+
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println();
+        System.out.println(listIterator);
+
+
     }
+
+
 }
