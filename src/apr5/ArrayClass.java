bbf365d3250cd03cf794c1c80a11968559350173
NikhilPal79 <nikhilchpr7@gmail.com>
2025-04-08 11:25:39 -0400

    5th apr

--- src/apr5/ArrayClass.java
+++ src/apr5/ArrayClass.java
@@ -9,15 +9,39 @@ public class ArrayClass {
 
         int[] arr =  {10,34,53,23,53,32,34,53,232,53,54323,43,34};
 
-        // using Arrays.as list method
+        Arrays.sort(arr);
 
-        List<Integer> list = Arrays.asList(10, 34, 53, 23, 53, 32, 34, 53, 232, 53, 54323, 43, 34);
 
-        ///  using
+        ///  above sort function will not give you output
 
-        int result = Arrays.binarySearch(arr, 232);
-///  returns the index
-        System.out.println(result);
+        System.out.println(Arrays.toString(arr));
+        System.out.println();
+
+        // using Arrays.as list method and then convert it in a variable
+
+        List<Integer> list = Arrays.asList(10, 23, 32, 34, 34, 34, 43, 53, 53, 53, 53, 232, 54323);
+
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println();
+        /*System.out.println(list.remove(Integer.valueOf(53)));*/ /// giving exception that they are unable to remove all 53 in list
+        System.out.println(list);
+        System.out.println();
+
+
+        /// using set to remove all 53
+
+        ///  convert list into set
+
+        Set<Integer> set = new TreeSet<>(list);
+
+        System.out.println(set);
+        System.out.println(set.size());
+        System.out.println();
+
+        System.out.println(set.remove(53));
+        System.out.println(set);
+        System.out.println(set.size());
 
 
     }
