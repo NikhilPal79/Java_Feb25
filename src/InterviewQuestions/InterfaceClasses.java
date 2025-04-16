f6a556975a917e27e529e89d6805e0ebbde6737e
NikhilPal79 <nikhilchpr7@gmail.com>
2025-04-15 21:32:03 -0400

    slides

---
+++ src/InterviewQuestions/InterfaceClasses.java
@@ -0,0 +1,30 @@
+package InterviewQuestions;
+
+public class InterfaceClasses {
+
+    /// INTERFACE CLASSES
+
+    /// 1.An interface is just like a class and specifies the behavior that a class must implement.
+
+    /// 2.An interface can be used to achieve 100% abstraction as it contains the method signatures/abstract methods
+    /// (what to be done) and no implementation details (how to be done) of these methods. In this way, interfaces
+    /// satisfy the definition of abstraction. The implementation techniques of the methods declared in an interface are
+    /// totally up to the classes implementing that interface.
+
+    /// 3.An interface can be thought of as a contract that a class has to fulfill while implementing an interface.
+    /// According to this contract, the class that implements an interface has to @Override all the abstract methods
+    /// declared in that very interface.
+
+
+
+    /// Key Features:
+    ///✅ Methods are abstract by default (until Java 8).
+
+    ///✅ All methods are public and abstract (unless they are static or default).
+
+    ///✅ Variables are public, static, and final by default.
+
+    ///✅ A class uses the implements keyword to implement an interface.
+
+    ///✅ Java supports multiple interface implementation, unlike classes (no multiple inheritance with classes
+}
