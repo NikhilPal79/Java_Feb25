dd3e2755ec16acbadd5dd314a0ec27cfb1d99652
NikhilPal79 <nikhilchpr7@gmail.com>
2025-04-15 21:24:38 -0400

    keywords

--- src/InterviewQuestions/KeywordSuper.java
+++ src/InterviewQuestions/KeywordSuper.java
@@ -1,4 +1,58 @@
 package InterviewQuestions;
 
 public class KeywordSuper {
+
+    ///  SUPER
+
+    /// The **super** keyword in Java is used to refer to the immediate parent class of the current object.
+    /// It has several important uses:
+
+
+    /// ✅ 1. Access Parent Class Constructor
+
+    /*class Parent {
+        Parent() {
+            System.out.println("Parent constructor");
+        }
+    }
+
+    class Child extends Parent {
+        Child() {
+            super(); // calls Parent class constructor
+            System.out.println("Child constructor");
+        }
+    }*/
+
+    /// ✅ 2. Access Parent Class Methods
+
+    /*class Parent {
+        void show() {
+            System.out.println("Parent show()");
+        }
+    }
+
+    class Child extends Parent {
+        void show() {
+            super.show(); // calls Parent's show()
+            System.out.println("Child show()");
+        }
+    }*/
+
+    /// ✅ 3. Access Parent Class Variables
+
+    /*class Parent {
+        int x = 10;
+    }
+
+    class Child extends Parent {
+        int x = 20;
+
+        void display() {
+            System.out.println("Child x = " + x);
+            System.out.println("Parent x = " + super.x);
+        }
+    }*/
+
+
+
 }
--- src/InterviewQuestions/KeywordThis.java
+++ src/InterviewQuestions/KeywordThis.java
@@ -1,4 +1,76 @@
 package InterviewQuestions;
 
 public class KeywordThis {
+    /// THIS
+
+    /// The **this** keyword in Java is a reference variable that refers to the current object
+    ///(i.e., the object on which the method or constructor is being called).
+
+
+   /// ✅ 1. Differentiate Between Instance and Local Variables
+
+   // When local variable names (like parameters) are the same as instance variables, this helps distinguish them.
+
+
+   class Student {
+       int id;
+
+       Student(int id) {
+           this.id = id;  // 'this.id' is instance variable, 'id' is parameter
+       }
+   }
+
+   /// ✅ 2. Call Another Constructor in the Same Class
+
+    // You can use this() to call another constructor in the same class (constructor chaining).
+
+   class Book {
+       String title;
+       double price;
+
+       Book() {
+           this("Java Book", 299.99); // calls parameterized constructor
+       }
+
+       Book(String title, double price) {
+           this.title = title;
+           this.price = price;
+       }
+   }
+
+   /// ✅ 3. Pass Current Object as a Parameter
+
+   class A {
+       void show(A obj) {
+           System.out.println("Object received");
+       }
+
+       void callShow() {
+           show(this);  // passing current object
+       }
+   }
+
+
+   /// ✅ 4. Return Current Object
+
+    // This is commonly used in method chaining.
+
+   class Person {
+       Person setName(String name) {
+           // set value
+           return this;
+       }
+
+       Person setAge(int age) {
+           // set value
+           return this;
+       }
+   }
+
+
+   /// Summary
+///this → refers to current object
+
+
+
 }
