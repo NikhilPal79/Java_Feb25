f95f54c27ed6e7761412b9f323f02a04d8d8b3d2
NikhilPal79 <nikhilchpr7@gmail.com>
2025-04-18 22:48:38 -0400

    apr17

---
+++ src/apr17/jenkins.java
@@ -0,0 +1,12 @@
+package apr17;
+
+public class jenkins {
+
+    /// CI/CD
+    ///  CONTINUOUS INTEGRATION/ CONTINUOUS DELIVERY
+
+    /// CORN JOB ===> A cron job is a Linux command used for scheduling tasks to be executed sometime in the future.
+    /// This is normally used to schedule a job that is executed periodically – for example, to send out a notice every morning.
+
+    /// INTERVIEW ====>
+}
