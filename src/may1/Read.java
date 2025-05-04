51fb943bcecc0d50a0537c44b53b21c6e693dd95
NikhilPal79 <nikhilchpr7@gmail.com>
2025-05-04 13:43:56 -0400

    may1

---
+++ src/may1/Read.java
@@ -0,0 +1,34 @@
+package may1;
+
+public class Read {
+
+    /*Database ?
+    ability to store the data
+
+    File system
+
+    Relational Database
+
+    Mysql, PostgreSQL , Oracle, SQL Server
+    Data stored in form of table .
+
+    Relationship two or more tables
+
+    Normalization - remove the redundant data , DBA 1N, 2, Bcnf , 4n , 5f
+
+    ACID properties A- Atomicity , transaction happens , it happens completely or not at all
+
+    transfer money from acount to another A - 1000 B
+
+    C - Consistency same state before and after transaction
+
+    I - Isolation independent transaction can happen
+
+    D - Durability during the system failure, changes in transacation happen
+
+    Blue Green Deployment
+
+            NoSQL
+    document type KEY - VALUE*/
+
+}
